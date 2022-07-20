2640390332c00a7a87ba9a7c0fc072cbdebf21c9
YHJ96 <yhj878812@naver.com>
2022-07-20 11:36:10 +0900

    feat: alert 목 함수 생성 완료

--- react-racingcar/src/setupTests.js
+++ react-racingcar/src/setupTests.js
@@ -3,6 +3,7 @@ import "@testing-library/jest-dom";
 const pickNumberInRange = jest.fn()
 
 beforeEach(() => {
+    global.alert = jest.fn();
     global.MissionUtils = {
         Random: {
             pickNumberInRange
