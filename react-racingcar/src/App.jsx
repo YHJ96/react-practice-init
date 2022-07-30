ac2bb06bfd5d28d2d144635af0e8eaec838fa0f4
YHJ96 <yhj878812@naver.com>
2022-07-30 16:49:41 +0900

    feat: 목업 수정 완료

--- react-racingcar/src/App.jsx
+++ react-racingcar/src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  /* 예시 state */
+  const [text, setText] = useState("");
+  const [number, setNumber] = useState(0);
+
   /* API 호출 상수 */
   const Random = global.MissionUtils.Random;
+  
   /* API 예시 입니다. 확인하시고 지우시면 됩니다. */
   const randomNumber = Random.pickNumberInRange(1, 10);
   console.log(randomNumber);
@@ -18,13 +23,21 @@ function App() {
         올바른 예) east,west,south,north <br />
       </p>
       <form>
-        <input type="text" />
-        <button>확인</button>
+        {/* input과 button에 핸들러를 만들어서 넣어주세요. */}
+        {/* value에 사용하고자 하는 state를 넣어주세요. */}
+        <input id="text-input" type="text" value={text} onChange={() => {}} />
+        <button id="text-button" type="submit" onClick={() => {}} >
+          입력
+        </button>
       </form>
       <h4>시도할 횟수를 입력해주세요.</h4>
       <form>
-        <input type="number" />
-        <button>확인</button>
+        {/* input과 button에 핸들러를 만들어서 넣어주세요. */}
+        {/* value에 사용하고자 하는 state를 넣어주세요. */}
+        <input id="number-input" type="number" value={number} onChange={() => {}} />
+        <button id="number-button" type="submit" onClick={() => {}} >
+          확인
+        </button>
       </form>
       <h4>📄 실행 결과</h4>
     </div>
