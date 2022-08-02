c05e3c802f9631715b937231ebc72eb940e47ed4
YHJ96 <yhj878812@naver.com>
2022-08-02 18:06:08 +0900

    feat: 잔돈 충전 탭 테스트 목록 작성중

---
+++ react-vendingmachine/src/__test__/Charge.test.js
@@ -0,0 +1,7 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Charge from '../pages/Charge';
+
+describe("🚀 random 테스트 케이스", () => {});
+describe("✅ input 테스트 케이스", () => {});
+describe("📀 잔돈 충전 탭 테스트 케이스", () => {});
